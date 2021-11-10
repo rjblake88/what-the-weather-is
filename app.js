8d17239a3dee33e6338274cca23108da31055158
Ryan J Blake <ryanjblake1988@gmail.com>
2021-11-09 16:18:10 -0800

    trying to fix local storage funct

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ button.addEventListener('click', function(){
     .then(data => {
         var nameValue = data['name'];
         var dateValue = data['date'];
-        var conditValue = data['weather'][0]['description, icon'];
+        var conditValue = data['weather'][0]['description'];
         var tempValue = data['main']['temp'];
         var humidValue = data['main']['humidity'];
         var windValue = data['wind'];
@@ -28,5 +28,35 @@ button.addEventListener('click', function(){
         wind.innerHTML =windValue;
         uv.innerHTML =uvValue;
     })
+    form.addEventListener('click', function (e) {
+        e.preventDefault()
+      
+        liMaker(input.value)
+        input.value = ''
+      })
 });
+
+const form = document.querySelector('form')
+const ul = document.querySelector('ul')
+let itemsArray = []
+
+localStorage.setItem('items', JSON.stringify(itemsArray))
+const data = JSON.parse(localStorage.getItem('items'))
+
+const liMaker = (text) => {
+    const li = document.createElement('li')
+    li.textContent = text
+    ul.appendChild(li)
+  }
+
+
+
+e.preventDefault()
+
+itemsArray.push(input.value)
+localStorage.setItem('items', JSON.stringify(itemsArray))
+
+data.forEach((item) => {
+    liMaker(item)
+})
 
